c7a14fa718109dc2b888bd5b49cdf21a26041196
aNodeWhizz <krish.440.singh@gmail.com>
2022-06-02 20:58:27 +0530

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: './src/lib/index.ts',
+      name: 'org-specs',
+      formats: ['es', 'umd'],
+      fileName: (format) => `org-specs.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
+  },
+});
